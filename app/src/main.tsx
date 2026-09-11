e928d2e11612c19ecda3da0b3d6aa0913696e427
xnuxi <research@alethic.ai>
2026-09-12 07:27:13 +0800

    Route /v2 to the new narrative research-page interface

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import AppV2 from './v2/AppV2';
 import './index.css';
+
+const isV2 = /\/v2\/?$/.test(window.location.pathname);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {isV2 ? <AppV2 /> : <App />}
   </React.StrictMode>
 );
