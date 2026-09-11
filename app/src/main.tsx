8342e9aee1466d6a9b811ee998c2a9f5f4890ab9
xnuxi <research@alethic.ai>
2026-09-12 07:49:14 +0800

    Route integrated research interface at /v3

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import AppV2 from './v2/AppV2';
+import AppV3 from './v3/AppV3';
 import './index.css';
 
-const isV2 = /\/v2\/?$/.test(window.location.pathname);
+const path = window.location.pathname;
+const isV3 = /\/v3\/?$/.test(path);
+const isV2 = /\/v2\/?$/.test(path);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {isV2 ? <AppV2 /> : <App />}
+    {isV3 ? <AppV3 /> : isV2 ? <AppV2 /> : <App />}
   </React.StrictMode>
 );
